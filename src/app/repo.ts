a300f4244f0a54e7585a042b32006a849cd1bdd9
PerisOduol618 <oduolpepe618@gmail.com>
2020-08-30 23:47:27 +0300

    Add a repo class

---
+++ src/app/repo.ts
@@ -0,0 +1,17 @@
+export class Repo {
+    constructor(
+        public name:string,
+        public description:string,
+        public repos:string,
+        public language :string,
+
+    ){
+        
+    }
+}
+
+
+
+
+
+
